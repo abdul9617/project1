4079e49c1a7dbaa450e25c5ad2acdbd27dd757fb
Abdul <abdul9617099@gmail.com>
2023-05-17 23:18:18 +0530

    first commit

---
+++ src/Employee/First.java
@@ -0,0 +1,9 @@
+package Employee;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("This is first program.");
+		
+	}
+}
---
+++ src/Employee/second.java
@@ -0,0 +1,29 @@
+package Employee;
+
+public class second {
+	
+		public static void main(String[] args) {
+
+			int n = 10;
+
+			float f = 1.478567346578367863f;
+
+			double d = 1.85976874878954687756878d;
+
+			char c = 'a';
+
+			boolean b = true;
+
+			String name = "kaushal";
+
+			System.out.println(n);
+
+			System.out.println(f);
+			System.out.println(d);
+			System.out.println(c);
+			System.out.println(b);
+			System.out.println(name);
+
+		}
+
+}
